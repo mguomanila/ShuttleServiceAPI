b1f9e9b666c2c2865cd2289514f90d1453c2ffff
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2019-04-02 10:22:38 +1300

    Added Unit Test for Payment
    Added 1 basic unit test for the payment route

---
+++ test/payment.js
@@ -0,0 +1,31 @@
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const server = require('../server')
+const should = chai.should()
+
+const paymentDetails = {
+	userID: 1,
+  transactionID: 'PAY-TEST123-FROMPAYPAL',
+  amount: 23
+}
+
+chai.use(chaiHttp)
+
+describe('*********** PAYMENT ***********', () => {
+	describe('/POST payment', () => {
+		it('it should POST payment data', done => {
+			chai
+				.request(server)
+				.post('/payment')
+				.send(paymentDetails)
+				.end((err, res) => {
+					res.should.have.status(200)
+          res.body.should.be.an('object')
+          res.body.should.have.property('tranactionID')
+					done()
+				})
+		})
+	})
+})
