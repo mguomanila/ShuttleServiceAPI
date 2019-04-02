05e7255ccdad8ee91bfdbd9c1a7b68eee634c781
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2019-04-02 18:51:53 +1300

    Fixed indentation

--- test/user.js
+++ test/user.js
@@ -6,29 +6,29 @@ const server = require('../server')
 const should = chai.should()
 
 const userDetails = {
-    userID: 1,
-    name: 'Michael Jeffcoat',
-    email: 'mwb8386@autuni.ac.nz',
-    studentId: 'mwb8386'
+	userID: 1,
+	name: 'Michael Jeffcoat',
+	email: 'mwb8386@autuni.ac.nz',
+	studentId: 'mwb8386'
 }
 
 chai.use(chaiHttp)
 
 describe('*********** USER ***********', () => {
-    describe('/POST user', () => {
-        it('it should POST user information', done => {
-            chai
-                .request(server)
-                .post('/user')
-                .send(userDetails)
-                .end((err, res) => {
-                    res.should.have.status(200)
-                    res.body.should.be.an('object')
-                    res.body.should.have.property('name')
-                    res.body.should.have.property('email')
-                    res.body.should.have.property('studentId')
-                    done()
-                })
-        })
-    })
+	describe('/POST user', () => {
+		it('it should POST user information', done => {
+			chai
+				.request(server)
+				.post('/user')
+				.send(userDetails)
+				.end((err, res) => {
+					res.should.have.status(200)
+					res.body.should.be.an('object')
+					res.body.should.have.property('name')
+					res.body.should.have.property('email')
+					res.body.should.have.property('studentId')
+					done()
+				})
+		})
+	})
 })
