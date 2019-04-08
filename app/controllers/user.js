b5eb96789794109d25658771376d17a0148e7b67
mikegjeff <mikegjeffcoat@gmail.com>
2019-04-09 09:32:24 +1200

    updated model

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -5,7 +5,14 @@ module.exports = {
         .create({
             first_name: req.body.first_name,
             last_name: req.body.last_name,
+            date_of_birth:req.body.date_of_birth,
+            gender:req.body.gender,
+            role_id:req.body.role_id,
+            email_address:req.body.email_address,
+            password:req.body.password,
             balance: req.body.balance,
+            university_id:req.body.university_id,
+            id_expiry:req.body.id_expiry,
         })
         .then(user => res.status(201).send(user))
         .catch(error => res.status(400).send(error));
