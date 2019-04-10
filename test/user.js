1925fe76561b1e5825f8e16278ce075aa3e36371
mikegjeff <38878555+mikegjeff@users.noreply.github.com>
2019-04-10 13:05:46 +1200

    improvedtestcasestestingforaddedfeatures

--- test/user.js
+++ test/user.js
@@ -4,26 +4,42 @@ const chai = require('chai')
 const chaiHttp = require('chai-http')
 const server = require('../server')
 const should = chai.should()
-
-const userDetails = {
-	first_name: 'Michael',
-	last_name: 'Jeffcoat',
-	balance: 500
-}
+const faker = require('faker')
 
 chai.use(chaiHttp)
 
-describe('*********** USER ***********', () => {
-	describe('/POST user', () => {
-		it('it should POST user information', done => {
-			chai
-				.request(server)
-				.post('/api/user')
-				.send(userDetails)
-				.end((err, res) => {
-					res.should.have.status(201)
-					done()
-				})
-		})
+describe('/POST user', () => {
+	it('it should NOT POST an empty User', done => {
+		const user = {}
+		chai
+			.request(server)
+			.post('/Users')
+			.send(user)
+			.end((err, res) => {
+				res.should.have.status(400)
+				done()
+			})
+	})
+	it('it should POST a user ', done => {
+		const user = {
+			points: faker.random.number(500),
+			first_name: faker.name.firstName(),
+			last_name: faker.name.lastName(),
+			date_of_birth: faker.date.past(20),
+			gender: "MALE",
+			role_id: faker.random.number(3),
+			email_address: faker.internet.email(),
+			password: faker.random.word(10),
+			university_id: faker.random.number(20000),
+			id_expiry: faker.date.future(2),
+		}
+		chai
+			.request(server)
+			.post('/Users')
+			.send(user)
+			.end((err, res) => {
+				res.should.have.status(201)
+				done()
+			})
 	})
 })
